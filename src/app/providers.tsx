15758ffa2a4140a359d8f4f4e4eee1e3ded8cd77
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-19 16:31:33 +0200

    add tanstack query

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { ReactNode } from "react";
+import { getQueryClient } from "./getQueryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function Providers({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
