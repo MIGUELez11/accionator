7b6250ce5f3628116e722baa5c0ece8524da9d8a
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-20 12:49:13 +0200

    add eslint and prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,35 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:jsx-a11y/recommended",
+      "plugin:import/recommended",
+      "plugin:import/typescript",
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
