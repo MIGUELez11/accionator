932f6ffb0170e7c6dbbc61da3e9672148fb6e679
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-20 14:40:08 +0200

    add react-scan

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,28 @@
-"use client";
+'use client';
 
-import { ReactNode } from "react";
-import { getQueryClient } from "./getQueryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, useEffect } from 'react';
+import { scan } from 'react-scan';
+import { getQueryClient } from './getQueryClient';
+
+function ReactScan() {
+  useEffect(() => {
+    scan({
+      enabled: true,
+    });
+  }, []);
+
+  return <></>;
+}
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactScan />
+
       {children}
       <ReactQueryDevtools />
     </QueryClientProvider>
