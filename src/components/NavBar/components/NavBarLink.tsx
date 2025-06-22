edaa4f45150dde35ab3a442e79571967a06d70ef
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-22 17:27:09 +0200

    remove unuseful hook

--- src/components/NavBar/components/NavBarLink.tsx
+++ src/components/NavBar/components/NavBarLink.tsx
@@ -1,44 +1,32 @@
-"use client";
+'use client';
 
-import { useCurrentPath } from "@/hooks/useCurrentPath";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
-import { useMemo } from "react";
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
 
 function cleanPath(path: string, href: string) {
-  const hrefParts = href.split("/");
-  const pathParts = path.split("/");
+  const hrefParts = href.split('/');
+  const pathParts = path.split('/');
 
   if (hrefParts.length !== pathParts.length) return path;
 
   return hrefParts
     .map((part, i) => {
-      if (part.startsWith(":")) {
+      if (part.startsWith(':')) {
         return part;
       }
       return pathParts[i];
     })
-    .join("/");
+    .join('/');
 }
 
-export function NavBarLink({
-  href,
-  children,
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
-  const currentPath = useCurrentPath();
-  const isActive = useMemo(
-    () => cleanPath(currentPath, href) === href,
-    [currentPath, href],
-  );
+export function NavBarLink({ href, children }: { href: string; children: React.ReactNode }) {
+  const currentPath = usePathname();
+  const isActive = useMemo(() => cleanPath(currentPath, href) === href, [currentPath, href]);
 
   return (
-    <Link
-      href={href}
-      className={cn({ "text-primary": isActive, "text-gray-500": !isActive })}
-    >
+    <Link href={href} className={cn({ 'text-primary': isActive, 'text-gray-500': !isActive })}>
       {children}
     </Link>
   );
