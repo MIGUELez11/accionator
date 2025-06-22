a417bb57529fbc2ddaf099f5e740d65d4398e348
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-22 17:22:44 +0200

    handle error and clean state handling

--- src/app/screener/page.tsx
+++ src/app/screener/page.tsx
@@ -1,28 +1,57 @@
-"use client";
+'use client';
 
-import { TitledSection } from "@/components/TitledSection";
-import { ScreenerPicker } from "./components/ScreenerPicker/ScreenerPicker";
-import { useState } from "react";
-import { ScreenerTable } from "./components/ScreenerTable";
-import { useQuery } from "@tanstack/react-query";
-import { screenerQuery } from "@/queries/screenerQuery";
+import { TitledSection } from '@/components/TitledSection';
+import { screenerQuery } from '@/queries/screenerQuery';
+import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { ScreenerPicker } from './components/ScreenerPicker/ScreenerPicker';
+import { ScreenerTable } from './components/ScreenerTable';
 
-export default function ScreenerPage() {
-  const [selectedScreener, setSelectedScreener] = useState<string | null>(null);
-  const { data, isLoading } = useQuery(screenerQuery(selectedScreener));
-
-  console.log("data", data);
-
+function Wrapper({
+  children,
+  selectedScreener,
+  setSelectedScreener,
+}: {
+  children: React.ReactNode;
+  selectedScreener: string | null;
+  setSelectedScreener: (screener: string) => void;
+}) {
   return (
     <TitledSection
       title="Descubre oportunidades de inversión"
       subtitle="Selecciona un screener para encontrar acciones con potencial de inversión a corto plazo"
     >
       <ScreenerPicker value={selectedScreener} onChange={setSelectedScreener} />
-      {selectedScreener && isLoading ? <div>Loading...</div> : null}
-      {selectedScreener && !isLoading && data ? (
-        <ScreenerTable data={data} />
-      ) : null}
+      {children}
     </TitledSection>
   );
+}
+
+export default function ScreenerPage() {
+  const [selectedScreener, setSelectedScreener] = useState<string | null>(null);
+  const { data, isLoading, error } = useQuery(screenerQuery(selectedScreener));
+
+  if (selectedScreener && isLoading) {
+    return (
+      <Wrapper selectedScreener={selectedScreener} setSelectedScreener={setSelectedScreener}>
+        <div>Loading...</div>
+      </Wrapper>
+    );
+  }
+
+  if (error) {
+    return (
+      <Wrapper selectedScreener={selectedScreener} setSelectedScreener={setSelectedScreener}>
+        <div>Something went wrong</div>
+      </Wrapper>
+    );
+  }
+
+  if (selectedScreener && !isLoading && !error && data) {
+    return (
+      <Wrapper selectedScreener={selectedScreener} setSelectedScreener={setSelectedScreener}>
+        <ScreenerTable data={data} />
+      </Wrapper>
+    );
+  }
 }
