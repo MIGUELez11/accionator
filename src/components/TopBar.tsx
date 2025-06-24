3ec5eff2fbf680420b6752f9e0f136b0340ae55b
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-24 20:32:27 +0200

    add clerk integration

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,16 +1,38 @@
-import { NavBar } from "./NavBar";
-import { Separator } from "./ui/separator";
+import { UserButton as ClerkUserButton, SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
+import { NavBar } from './NavBar';
+import { Separator } from './ui/separator';
+
+function UserButton() {
+  return (
+    <div className="flex justify-end items-center p-4 gap-4 h-16">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <ClerkUserButton />
+      </SignedIn>
+    </div>
+  );
+}
 
 export function TopBar() {
   return (
-    <div>
-      <div className="flex flex-row items-center gap-6 h-16 min-w-full">
-        <div className="flex flex-row items-center gap-6 max-w-7xl w-full mx-auto px-4">
-          <p className="text-xl font-semibold">ACCIONATOR</p>
-          <NavBar />
+    <header>
+      <div className="flex flex-row items-center justify-between w-full">
+        <div className="flex flex-row items-center gap-6 h-16">
+          <div className="flex flex-row items-center gap-6 max-w-7xl w-full mx-auto px-4">
+            <p className="text-xl font-semibold">ACCIONATOR</p>
+            <NavBar />
+          </div>
         </div>
+        <UserButton />
       </div>
       <Separator />
-    </div>
+    </header>
   );
 }
