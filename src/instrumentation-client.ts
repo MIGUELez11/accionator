379a7c181f2964ae3652143f7888c8a3e2003a2e
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-25 16:20:23 +0200

    add posthog

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,9 @@
+import posthog from 'posthog-js';
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: '/relay-hedgehog',
+  capture_pageview: 'history_change',
+  capture_pageleave: true, // Enable pageleave capture
+  capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+  debug: process.env.NODE_ENV === 'development',
+});
