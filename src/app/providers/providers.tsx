5fe673db043674d7f62587e054b8e7e37611109b
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-06-25 12:16:54 +0200

    feat: add convex support with clerk integration
    Refs: #8

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,33 +1,23 @@
 'use client';
 
-import { ClerkProvider } from '@clerk/nextjs';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ReactNode, useEffect } from 'react';
-import { scan } from 'react-scan';
-import { getQueryClient } from './getQueryClient';
-
-function ReactScan() {
-  useEffect(() => {
-    scan({
-      enabled: true,
-    });
-  }, []);
-
-  return <></>;
-}
+import { ReactNode } from 'react';
+import { getQueryClient } from '../getQueryClient';
+import { ConvexClientProvider } from './ConvexProvider';
+import { ReactScan } from './ReactScan';
 
 export default function Providers({ children }: { children: ReactNode }) {
   const queryClient = getQueryClient();
 
   return (
-    <ClerkProvider>
+    <ConvexClientProvider>
       <QueryClientProvider client={queryClient}>
         <ReactScan />
 
         {children}
         <ReactQueryDevtools />
       </QueryClientProvider>
-    </ClerkProvider>
+    </ConvexClientProvider>
   );
 }
