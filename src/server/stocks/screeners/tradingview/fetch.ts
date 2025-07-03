16c40b8aab49135e318e38972c1d36f026536294
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-07-03 22:46:50 +0200

    remove User-Agen from TradingView API call to attach to TOS

--- src/server/stocks/screeners/tradingview/fetch.ts
+++ src/server/stocks/screeners/tradingview/fetch.ts
@@ -14,40 +14,41 @@ export const fetchTradingViewScreener = Effect.fn(function* ({
   filter: Filter[];
   symbols?: Symbols;
 }) {
-  const response = yield* Effect.tryPromise({
-    try: () =>
-      fetch(URL, {
-        method: 'POST',
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0',
-          Accept: 'application/json',
-          'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
-          'Content-Type': 'text/plain;charset=UTF-8',
-        },
-        referrer: 'https://www.tradingview.com/',
-        body: JSON.stringify({
-          columns,
-          filter,
-          symbols,
-          markets: ['america'],
-          range: [0, 100],
-          sort: {
-            sortBy: 'beta_1_year',
-            sortOrder: 'desc',
+  const response =
+    yield *
+    Effect.tryPromise({
+      try: () =>
+        fetch(URL, {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Accept-Language': 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Content-Type': 'text/plain;charset=UTF-8',
           },
-          options: {
-            lang: 'en',
-          },
-          filter2: defaultFilter2,
+          referrer: 'https://www.tradingview.com/',
+          body: JSON.stringify({
+            columns,
+            filter,
+            symbols,
+            markets: ['america'],
+            range: [0, 100],
+            sort: {
+              sortBy: 'beta_1_year',
+              sortOrder: 'desc',
+            },
+            options: {
+              lang: 'en',
+            },
+            filter2: defaultFilter2,
+          }),
         }),
-      }),
-    catch: (error) => {
-      return new TradingViewFetchError({
-        message: 'Failed to fetch trading view screener',
-        cause: error,
-      });
-    },
-  });
+      catch: (error) => {
+        return new TradingViewFetchError({
+          message: 'Failed to fetch trading view screener',
+          cause: error,
+        });
+      },
+    });
 
   const data = yield* Effect.tryPromise({
     try: () => response.json(),
