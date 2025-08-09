b3c68b8a3c48bc9bcd5c8da70f7ec9c0ccdc5b09
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-08-09 18:33:09 +0200

    add handler for handling shift click for quick actions

---
+++ src/lib/handleShiftClick.ts
@@ -0,0 +1,16 @@
+interface HandleShiftClickProps {
+  onClick?: () => void;
+  onShiftClick?: () => void;
+}
+
+export function handleShiftClick({ onClick, onShiftClick }: HandleShiftClickProps) {
+  return (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.shiftKey && typeof onShiftClick === 'function') {
+      e.preventDefault();
+      onShiftClick?.();
+    } else if (typeof onClick === 'function') {
+      e.preventDefault();
+      onClick();
+    }
+  };
+}
