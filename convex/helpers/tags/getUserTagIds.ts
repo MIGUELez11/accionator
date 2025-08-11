5fe5abd54678f46129c0c2377947c63c4f2be0bf
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-08-11 21:40:59 +0200

    dedupe duplicated tag ids in getTagIdsHelper

--- convex/helpers/tags/getUserTagIds.ts
+++ convex/helpers/tags/getUserTagIds.ts
@@ -1,13 +1,20 @@
-import { DataModel } from "@convex/_generated/dataModel";
-import { GenericQueryCtx } from "convex/server";
+import { DataModel } from '@convex/_generated/dataModel';
+import { GenericQueryCtx } from 'convex/server';
 
-
-async function getTagId(ctx: GenericQueryCtx<DataModel>, { userId, tag }: { userId: string, tag: string }) {
-  const tagDoc = await ctx.db.query('operationTags').withSearchIndex('by_tag', (q) => q.search('tag', tag).eq('userId', userId)).first();
+async function getTagId(ctx: GenericQueryCtx<DataModel>, { userId, tag }: { userId: string; tag: string }) {
+  const tagDoc = await ctx.db
+    .query('operationTags')
+    .withSearchIndex('by_tag', (q) => q.search('tag', tag).eq('userId', userId))
+    .first();
 
   return tagDoc?._id ?? null;
 }
 
-export function getTagIdsHelper(ctx: GenericQueryCtx<DataModel>, { userId, tags }: { userId: string, tags: string[] }) {
-  return Promise.all(tags.map((tag) => getTagId(ctx, { userId, tag })));
+export async function getTagIdsHelper(
+  ctx: GenericQueryCtx<DataModel>,
+  { userId, tags }: { userId: string; tags: string[] },
+) {
+  const tagIds = await Promise.all(tags.map((tag) => getTagId(ctx, { userId, tag })));
+
+  return [...new Set(tagIds)];
 }
