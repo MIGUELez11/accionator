413267163ab3cb6dab3a61d9842611b27712c1a7
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-08-11 21:43:58 +0200

    remove unnecessary use client directive

--- src/app/(protected)/operations/components/Symbol/SymbolTab.tsx
+++ src/app/(protected)/operations/components/Symbol/SymbolTab.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Suspense } from 'react';
 import { PortfolioPerformance } from '../PortfolioPerformance';
 import { PortfolioPerformanceSkeleton } from '../PortfolioPerformanceSkeleton';
