6e57d678703476d9debde7ef7bede1967580c747
Miguel Ángel Florido Cifuentes <miguelez11games@gmail.com>
2025-08-11 21:22:06 +0200

    return ctx.db.insert so it can be awaited

--- convex/helpers/operation/add.ts
+++ convex/helpers/operation/add.ts
@@ -1,8 +1,11 @@
-import { DataModel, Doc } from "@convex/_generated/dataModel";
-import { GenericMutationCtx, WithoutSystemFields } from "convex/server";
+import { DataModel, Doc } from '@convex/_generated/dataModel';
+import { GenericMutationCtx, WithoutSystemFields } from 'convex/server';
 
-export function addOperationHelper(ctx: GenericMutationCtx<DataModel>, { userId, operation }: { userId: string; operation: Omit<WithoutSystemFields<Doc<"operations">>, "userId"> }) {
-  ctx.db.insert('operations', {
+export function addOperationHelper(
+  ctx: GenericMutationCtx<DataModel>,
+  { userId, operation }: { userId: string; operation: Omit<WithoutSystemFields<Doc<'operations'>>, 'userId'> },
+) {
+  return ctx.db.insert('operations', {
     ...operation,
     userId,
   });
